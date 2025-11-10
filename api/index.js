2504da554704279b8fa18111b4ae164f376d510c
Amer <amerlopo5@gmail.com>
2025-11-10 16:41:51 +0100

    prod

--- api/index.js
+++ api/index.js
@@ -11,12 +11,10 @@ app.use(helmet());
 app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-
 app.use("/", routes);
-
 app.use((_req, res) => {
   res.status(404).json({ error: "Route not found" });
 });
 
-// Named export koji Vercel očekuje
-export const handler = serverless(app);
+// ✅ ključ: export default handler
+export default serverless(app);
