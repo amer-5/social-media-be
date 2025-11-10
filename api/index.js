08c03385e720d2023e00153dfe92ec04339bd320
Amer <amerlopo5@gmail.com>
2025-11-10 17:08:21 +0100

    prod

--- api/index.js
+++ api/index.js
@@ -1,20 +1,22 @@
 import express from "express";
+import serverless from "serverless-http";
+import routes from "../routes/index.js";
 import cors from "cors";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
-import routes from "../routes/index.js";
-import serverless from "serverless-http";
 
 const app = express();
 
 app.use(helmet());
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/", routes);
+
+// 404 fallback
 app.use((_req, res) => {
   res.status(404).json({ error: "Route not found" });
 });
 
-// ✅ ključ: export default handler
+// ✅ Default export serverless handler
 export default serverless(app);
