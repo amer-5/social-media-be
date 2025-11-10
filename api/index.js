25bc1586c3aca4d3c98034dc854842a8b25f7326
Amer <amerlopo5@gmail.com>
2025-11-10 16:01:47 +0100

    prod

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import routes from "../routes/index.js";
+import routes from "../routes";
 import cookieParser from "cookie-parser";
 import serverless from "serverless-http";
 
