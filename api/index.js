8514c3502fb54921067c2f45edb3525506b5da4f
Amer <amerlopo5@gmail.com>
2025-11-10 15:56:18 +0100

    .

--- api/index.js
+++ api/index.js
@@ -7,19 +7,15 @@ import serverless from "serverless-http";
 
 const app = express();
 
-// Middleware
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-// Routes
 app.use("/", routes);
 
-// 404 handler
 app.use((_req, res) => {
   res.status(404).send("Route not found");
 });
 
-// Export serverless handler
-export const handler = serverless(app);
+export default serverless(app);
